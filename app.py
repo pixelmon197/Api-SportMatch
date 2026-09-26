cb6a59e1ddd5cd8c2456c472a200d57f89295e5d
Omar Yova Aviles <56742347+Yoval128@users.noreply.github.com>
2026-09-25 18:05:19 -0600

    Remove redundant app initialization

--- app.py
+++ app.py
@@ -70,7 +70,6 @@ def create_app(config_class=Config):
 
     return app
 
-
+app = create_app()
 if __name__ == "__main__":
-    app = create_app()
     app.run(host="0.0.0.0", debug=True, port=5000)
